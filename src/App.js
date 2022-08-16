b663efacdfde97719c695e0d279883f8793ee196
RohSungKyun <songkyun1108@naver.com>
2022-08-16 23:11:46 +0900

    react-router-rom

--- src/App.js
+++ src/App.js
@@ -1,37 +1,20 @@
-import { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+// v6 react-router0dom (https://reactrouter.com/docs/en/v6/getting-started/overview)
+// Sitch => Routes, component => element로 바뀜 (https://velog.io/@kcdoggo/Switch-is-not-exported-from-react-router-dom-%EC%97%90%EB%9F%AC)링크
 
 function App() {
-  const [loading, setLoading] = useState(true)
-  const [movies, setMovies] = useState([])
-  const getMovies = async() => {
-    const json = await (
-      await fetch(
-      'https://yts.mx/api/v2/list_movies.json?minimum_rating=9.0&sort_by=year'
-    )).json();
-    
-    setMovies(json.data.movies);
-    setLoading(false);
-  }
-  useEffect(()=>{
-    getMovies()
-  }, []);
-  console.log(movies);
-  return <div>
-    {loading ?
-     <h1>Loading...</h1>
-      : <div>
-        {movies.map(movie => 
-          <div key={movie.id}>
-            <img src={movie.medium_cover_image} />
-            <h2>{movie.title}</h2>
-            <p>{movie.summary}</p>
-            <ul>
-              {movie.genres.map((g) => (
-                <li key={g}>{g}</li>
-              ))}
-            </ul>
-          </div>
-        )}</div>}</div>;
+  return <Router>
+    <Routes> 
+      <Route path="/" element={<Home />} />
+      <Route path="/movie" element={<Detail />} />
+    </Routes>
+  </Router>;
 }
 
 export default App;
